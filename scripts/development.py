320fae4c5c3c119a6707439b666e1658dd7ec9ad
nicksunderland <nicholas.p.sunderland@gmail.com>
2023-02-09 23:39:44 +0000

    moved scripts out of py project

---
+++ scripts/development.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import importlib
+for mod in [".".join(["datamanagerpy", n]) for n in ['data_file', 'cache', 'pkg_utils']]:
+  if mod in sys.modules:
+    importlib.reload(sys.modules[mod])
+from datamanagerpy.cache import *
+from datamanagerpy.data_file import *
+from datamanagerpy.pkg_utils import *
+
+#set_config("download_dir_path", "/Users/nicholassunderland/git/thesis/datamanager/downloads")
+
+DataFile.delete("hermes")
+
+data_file = DataFile(
+  name="hermes",
+  source="https://personal.broadinstitute.org/ryank/HERMES_Jan2019_HeartFailure_summary_data.txt.zip")
+
+data_file.get("HERMES_Jan2019_HeartFailure_summary_data.txt")
